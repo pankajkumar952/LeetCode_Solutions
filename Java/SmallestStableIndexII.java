42ad5514b6ececaa1e0d4e41a2915062c9a4905e
Pankaj Kumar <pankjku5698@gmail.com>
2026-09-05 11:31:29 +0530

    SmallestStableIndexII.java

---
+++ Java/SmallestStableIndexII.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] minRight = new int[n];
+        minRight[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            minRight[i] = Math.min(nums[i], minRight[i + 1]);
+        }
+
+        int maxLeft = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            maxLeft = Math.max(maxLeft, nums[i]);
+
+            if (maxLeft - minRight[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
